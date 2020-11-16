41043ca09110901c90a7b8dba8248cb99ae74202
megha1399 <54594703+megha1399@users.noreply.github.com>
2020-11-16 21:57:26 +0530

    Update app.module.ts

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import {MatTableModule} from '@angular/material/table'; 
+import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
